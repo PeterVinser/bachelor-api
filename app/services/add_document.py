d20c8fa151feb4bed5eb07c5efc578fd8d677372
PiotrOk <piotr.okninski800@gmail.com>
2024-05-02 14:51:13 +0200

    separated uploader logic

--- app/services/add_document.py
+++
@@ -1,6 +0,0 @@
-from flask import jsonify
-
-def handle_add_document(request):
-    document_name = request.json['documentName']
-    
-    return jsonify({"result": f"document {document_name} added"})
